0848b25a0c900531e06e650e3c4966e3205ef050
MB-study <MB-study@192.168.1.51>
2020-11-17 21:05:11 +0800

    Java

---
+++ src/main/java/com/utils/MathUtil.java
@@ -0,0 +1,16 @@
+
+package com.utils;
+
+
+public class MathUtil {
+    public static double sum(double[] values) {
+        int sum = 0;
+        for (int i = 0; i < values.length; i++) {
+            sum += values[i];  
+        }
+        return sum;
+    }
+    public static double avg(double[] values) {
+        return sum(values) / values.length;
+    }
+}
