2ea316595160f79132755d5231308f2874676c62
MB-study <MB-study@DESKTOP-KVQTFGR>
2020-12-17 21:39:51 +0800

    Java

--- src/main/java/com/ocp12/lambda3/Main.java
+++ src/main/java/com/ocp12/lambda3/Main.java
@@ -1,5 +1,7 @@
 package com.ocp12.lambda3;
 
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
         int score =65;
@@ -10,6 +12,13 @@ public class Main {
         
         int foodScore =100;
         checkScore(foodScore, (x) -> x ==100);
+        
+        //使用 Predicate
+        // boolean  test (T t)
+        Predicate<Double> pass=(x)-> x==0;
+        System.out.println(pass.test(0.18));
+        Predicate<Double> bmipass=(x)-> x>=18 && x<23;
+        System.out.println(bmipass.test(21.76));
     }
     public static void checkScore(int x,Pass p){
         System.out.printf("%d 的判斷結果==> %b\n",x,p.check(x));
