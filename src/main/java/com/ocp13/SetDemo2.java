31c50c0e8c506da84a8e32fdb46be5b9c9c48ba7
MB-study <MB-study@DESKTOP-KVQTFGR>
2020-12-22 19:35:08 +0800

    Java

--- src/main/java/com/ocp13/SetDemo2.java
+++ src/main/java/com/ocp13/SetDemo2.java
@@ -1,12 +1,14 @@
 package com.ocp13;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Random;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetDemo2 {
     public static void main(String[] args) {
-        Set set =new HashSet();
+        Set set =new TreeSet();
         // 今彩 539 , 1~39取五個不重複的數字
         // 電腦選號
         while (set.size() < 5) {            
