bf71c08f6a3e158b91be3eeb7db18037a7d96872
MB-study <MB-study@DESKTOP-KVQTFGR>
2020-12-22 21:40:57 +0800

    Java

--- src/main/java/com/ocp13/SetDemo4.java
+++ src/main/java/com/ocp13/SetDemo4.java
@@ -19,6 +19,8 @@ public class SetDemo4 {
                 .average()
                 .getAsDouble();
         System.out.println("國文平均:" + avg_Chinese);
+        System.out.println("==========================================================");
+
         System.out.println("2.英文平均");
         exams.stream().mapToInt(e -> e.getEnglish())
                 .forEach(score -> System.out.print(score + " ,"));
@@ -28,6 +30,8 @@ public class SetDemo4 {
                 .average()
                 .getAsDouble();
         System.out.println("英文平均:" + avg_English);
+        System.out.println("==========================================================");
+
         System.out.println("3.數學平均");
         exams.stream().mapToInt(e -> e.getMath())
                 .forEach(score -> System.out.print(score + " ,"));
@@ -37,6 +41,28 @@ public class SetDemo4 {
                 .average()
                 .getAsDouble();
         System.out.println("數學平均:" + avg_Math);
-        
-     }
+        System.out.println("==========================================================");
+
+        System.out.println("4.國文最高分為?");
+        int max_Chinese = exams.stream()
+                .mapToInt(e -> e.getChinese())
+                .max()
+                .getAsInt();
+        System.out.println("國文最高分為:" + max_Chinese);
+
+        System.out.print("國文最高分的人是誰?");
+        exams.stream()
+                .filter(e -> e.getChinese() == max_Chinese)
+                .forEach(e -> System.out.print(e.getName() + ","));
+        System.out.println();
+
+        System.out.println("6.總成績最高分的人名為何? 是幾分?");
+        int max_score = exams.stream()
+                .mapToInt(e -> e.getChinese() + e.getEnglish() + e.getMath())
+                .max()
+                .getAsInt();
+
+        exams.stream().filter(e -> e.getChinese() + e.getEnglish() + e.getMath() == max_score)
+                .forEach(e -> System.out.println(e.getName() + "," + max_score));
+    }
 }
