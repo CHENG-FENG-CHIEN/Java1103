14f891a14e006c87e9582b2256b4128c133e207b
MB-study <MB-study@DESKTOP-KVQTFGR>
2020-12-29 21:46:37 +0800

    Java

--- src/main/java/com/ocp15/GroupByDemo2.java
+++ src/main/java/com/ocp15/GroupByDemo2.java
@@ -10,31 +10,31 @@ public class GroupByDemo2 {
 
     public static void main(String[] args) {
         List<Fruit> fruits = Arrays.asList(
-                new Fruit("apple"     , 10, 10),
-                new Fruit("orange"    , 15, 15),
-                new Fruit("apple"     , 20, 10),
-                new Fruit("banana"    , 10, 15),
+                new Fruit("apple", 10, 10),
+                new Fruit("orange", 15, 15),
+                new Fruit("apple", 20, 10),
+                new Fruit("banana", 10, 15),
                 new Fruit("watermelon", 10, 20),
-                new Fruit("papaya"    , 15, 15),
-                new Fruit("papaya"    , 20, 15),
-                new Fruit("apple"     , 20, 10)
+                new Fruit("papaya", 15, 15),
+                new Fruit("papaya", 20, 15),
+                new Fruit("apple", 20, 10)
         );
         //每一種水果有幾組?
         //e -> e.getName() 可寫成 Fruit::getName
         Map<String, Long> counting = fruits.stream()
-                  .collect(Collectors.groupingBy(Fruit::getName, Collectors.counting()));
+                .collect(Collectors.groupingBy(Fruit::getName, Collectors.counting()));
         System.out.println(counting);
         //每一種水果有幾個?
-        Map<String ,Integer> sum = fruits.stream()
-                        .collect(Collectors.groupingBy(Fruit::getName, Collectors.summingInt(Fruit::getQty)));
+        Map<String, Integer> sum = fruits.stream()
+                .collect(Collectors.groupingBy(Fruit::getName, Collectors.summingInt(Fruit::getQty)));
         System.out.println(sum);
         //根據價錢來分組
-        Map<Integer,List<Fruit>> groupingByPrice = fruits.stream()
+        Map<Integer, List<Fruit>> groupingByPrice = fruits.stream()
                 .collect(Collectors.groupingBy(Fruit::getPrice));
         System.out.println(groupingByPrice);
-          //根據價錢來分組(水果名不可重複)
-        Map<Integer , Set<String>> groupByPrice2 = fruits.stream()
-                .collect(Collectors.groupingBy(Fruit::getPrice , Collectors.mapping(Fruit::getName, Collectors.toSet())));
+        //根據價錢來分組(水果名不可重複)
+        Map<Integer, Set<String>> groupByPrice2 = fruits.stream()
+                .collect(Collectors.groupingBy(Fruit::getPrice, Collectors.mapping(Fruit::getName, Collectors.toSet())));
         System.out.println(groupByPrice2);
     }
 }
