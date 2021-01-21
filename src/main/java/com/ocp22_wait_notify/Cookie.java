52e7299bf49966bbd653805baf60df433b5c5e03
MB-study <MB-study@DESKTOP-KVQTFGR>
2021-01-21 19:13:14 +0800

    Java

---
+++ src/main/java/com/ocp22_wait_notify/Cookie.java
@@ -0,0 +1,18 @@
+package com.ocp22_wait_notify;
+
+public class Cookie {
+    
+    // 吃餅乾
+    public void eat(int no){
+        String name =Thread.currentThread().getName();
+        System.out.printf("%s 吃了第 %d 塊餅乾",name,no);
+    }
+    
+    
+    // 放餅乾
+    public void put(int no){
+        String name =Thread.currentThread().getName();
+        System.out.printf("%s 放了第 %d 塊餅乾",name,no);
+    }
+    
+}
